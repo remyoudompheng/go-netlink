37f4f363aa5e22e0307bdeb4c54d764f15f1e91d
Rémy Oudompheng <remy@archlinux.org>
2011-08-07 19:52:46 +0200

    netlink: add generic messages to test
    Signed-off-by: Rémy Oudompheng <remy@archlinux.org>

--- test.go
+++ test.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"os"
 	"bufio"
+	"bytes"
 	"fmt"
 	"netlink"
 	"syscall"
@@ -33,6 +35,27 @@ func TestRouteAddr(r *bufio.Reader, w *bufio.Writer) {
 	}
 }
 
+func TestGenericFamily(r *bufio.Reader, w *bufio.Writer) {
+	msg := netlink.MakeGenCtrlCmd(netlink.CTRL_CMD_GETFAMILY)
+	netlink.WriteMessage(w, &msg)
+	b := bytes.NewBuffer([]byte{})
+	buf := bufio.NewWriter(b)
+	netlink.WriteMessage(buf, &msg)
+	fmt.Println(b.Bytes())
+	fmt.Printf("%#v\n", msg)
+
+	for {
+		resp, _ := netlink.ReadMessage(r)
+		parsedmsg, _ := netlink.ParseGenlFamilyMessage(resp)
+		switch m := parsedmsg.(type) {
+		case netlink.ErrorMessage:
+			fmt.Printf("%#v, %s\n", m, os.NewSyscallError("netlink", int(-m.Errno)))
+			break
+		default:
+			fmt.Printf("%#v\n", m)
+		}
+	}
+}
 
 func main() {
 	s, _ := netlink.DialNetlink("route", 0)
@@ -40,4 +63,11 @@ func main() {
 	w := bufio.NewWriter(s)
 	TestRouteLink(r, w)
 	TestRouteAddr(r, w)
+
+	// NETLINK_GENERIC tests
+	s, _ = netlink.DialNetlink("generic", 0)
+	r = bufio.NewReader(s)
+	w = bufio.NewWriter(s)
+	fmt.Println("Testing generic family messages")
+	TestGenericFamily(r, w)
 }
